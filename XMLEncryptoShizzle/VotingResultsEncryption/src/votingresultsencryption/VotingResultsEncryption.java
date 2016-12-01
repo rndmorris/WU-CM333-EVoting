65d032962d526e7da02d38c272999346a1317104
Ben Ciummo <benciummo@gmail.com>
2016-12-01 10:22:52 -0600

    Finished decryption method on encryptoshizzle also cleaned up cmd args

--- XMLEncryptoShizzle/VotingResultsEncryption/src/votingresultsencryption/VotingResultsEncryption.java
+++ XMLEncryptoShizzle/VotingResultsEncryption/src/votingresultsencryption/VotingResultsEncryption.java
@@ -2,11 +2,7 @@
 
 package votingresultsencryption;
 
-//import java.awt.RenderingHints.Key;
-import java.io.File;
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -14,7 +10,6 @@ import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
- 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -26,17 +21,31 @@ public class VotingResultsEncryption
     public static void main(String[] args) 
             throws FileNotFoundException
     {
-        String inFilePath = args[0];
-        String outFilePath = args[1];
-        String key = args[2];
+        if(args.length < 4)
+        {
+            System.out.println("VotingResultsEncryption <encrypt|decrypt> <InFilePath> <OutFilePath> <16 digit Encryption Key>");
+            System.exit(1);
+        }
+        
+        String mode = args[0];
+        String inFilePath = args[1];
+        String outFilePath = args[2];
+        String key = args[3];
         File inFile = new File(inFilePath);
         File outFile = new File(outFilePath);
         
-        System.out.printf("this is a test input key as first arg and remove hard coded test key");
-        System.out.print(inFilePath);
-        encrypt("1111111111876789",
-                inFile,outFile);
         
+        
+        if(mode.toLowerCase().equals("encrypt"))
+        {
+            System.out.println("Encrypting " + inFile +" with key: "+ key+ " outputting to "+outFile);
+            encrypt(key,inFile, outFile);
+        }
+        else
+        {
+            System.out.println("Decrypting " + inFile +" with key: "+ key+ " outputting to "+outFile);
+            decrypt(key,inFile, outFile);
+        }        
     }
 
     private static void encrypt(String inKey, File inFile, File outFile) 
@@ -69,11 +78,36 @@ public class VotingResultsEncryption
         {
                System.out.println(ex);
         }
-        
     }
     
-    private static void decrypt(String key, String inFile, String outFile)
+    private static void decrypt(String inKey, File inFile, File outFile)
     {
-        
+        try
+        {
+            //Initialize an encryption stream
+            Key key = new SecretKeySpec(inKey.getBytes(),"AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            
+            //Pass input file to encryption stream
+            FileInputStream inputStream = new FileInputStream(inFile);
+            byte[] inputBytes = new byte[(int) inFile.length()];
+            inputStream.read(inputBytes);
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+            
+            //Take encrypted output stream and write to byte
+            FileOutputStream outputStream = new FileOutputStream(outFile);
+            outputStream.write(outputBytes);
+            
+            //Close Filestreams
+            inputStream.close();
+            outputStream.close();
+        }
+        catch(NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | IOException 
+                | IllegalBlockSizeException| BadPaddingException ex)
+        {
+               System.out.println(ex);
+        }
     }
 }
