9cdc6632d7ba414c4e166e5343a854968ece7139
shannon594 <shannon.hoffman@washburn.edu>
2016-12-08 12:52:28 -0600

    Update reg_end.php

--- voting_system/reg_end.php
+++ voting_system/reg_end.php
@@ -26,7 +26,7 @@
     $_SESSION['fromExit'] = 1;
 
 
-    $mysqli = new mysqli("127.0.0.1", "root", "password", "registration", 3306);
+    $mysqli = new mysqli("localhost", "root", "", "registration", 3306);
     if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
