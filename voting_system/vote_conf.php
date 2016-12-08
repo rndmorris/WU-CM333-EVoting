53bfeceb2b836e6e6c12a1785865ac119c022448
shannon594 <shannon.hoffman@washburn.edu>
2016-12-08 13:29:23 -0600

    Update vote_conf.php

--- voting_system/vote_conf.php
+++ voting_system/vote_conf.php
@@ -100,97 +100,61 @@
 
           if ( file_exists('xml/ballot.xml') ) {
 	    $xml = simplexml_load_file('xml/ballot.xml');
-      	//echo 'Races voted in: ', $_SESSION['ses_raceCnt'];
-      	//echo '<br>';
 
-      		/*DO THIS FOR REAL*/
-      	   // $scope = 0;
         echo '<h2>Your Vote:</h2>';
 
       	for($i = 0; $i < 100; $i++){
-      		/*echo 'in loop';*/
       		$r = strval($i);
-      		/*echo $r;
-      		echo '<br>';*/
       		$r = "r".$r;
-      		/*echo $r;
-      		echo '<br>';*/
 
 
      	if(isset($_POST[$r]))
      	{
-     		//echo 'IN IF';
-     		//echo $_POST[$r];
-        //echo "    the r =  ";
-        //echo $r;
      		$_SESSION[$r] = $_POST[$r];
-     		//echo $_SESSION[$r];
-        //echo "   ", $r;
+
         $scopeCnt2 = $xml->ballot[$district]->scopes->scope->count();
         echo '<br>';
         for( $sc = 0; $sc < $scopeCnt2; $sc++) {
-        //echo '<h4>';
-        //echo $xml->ballot[$district]->scopes->scope[$sc]->scope_name;
-        //echo '</h4>';
+
         $raceCnt2 = $xml->ballot[$district]->scopes->scope[$sc]->races->race->count();
      		for($t = 0; $t < $raceCnt2; $t++)
      		{
-     			/*echo 't ', $t;*/
      			$temp_raceid = $xml->ballot[$district]->scopes->scope[$sc]->races->race[$t]->race_id;
           $temp_racenm = $xml->ballot[$district]->scopes->scope[$sc]->races->race[$t]->race_name;
-     			/*echo 'in first for';*/
+     			
      			if($temp_raceid == $r)
      			{
             echo '<label class="control-label col-sm-4">';
             echo $temp_racenm;
             echo ': </label>';
-            //echo 'OUTSIDE!   ';
+           
      				$candCnt2 = $xml->ballot[$district]->scopes->scope->races->race[$t]->candidate->count();
      				for($n = 0; $n < $candCnt2; $n++)
      				{
-     					//echo '   n;   ', $n;
-     					/*echo ' in second for';*/
-              //echo '<div class="col-sm-5"';
      					$temp_id = $xml->ballot[$district]->scopes->scope[$sc]->races->race[$t]->candidate[$n]->id;
-              //echo ' tempID: '.$temp_id;
-              //echo '';
-              //echo ' sessR: '.$_SESSION[$r].'    ';
+ 
      					if($temp_id == $_SESSION[$r])
-              //if (substr( $_SESSION[$r], 0, 1 ) != "_")
+              
      					{
-               // echo '<div class="col-sm-5"';
-              //echo ' first if: ';
-              //echo $_SESSION[$r];
-              //break;
      					echo $xml->ballot[$district]->scopes->scope[$sc]->races->race[$t]->candidate[$n]->name;
               $writeInClear = 'wi_'.$r;
               unset($_SESSION[$writeInClear]);
               break;
-              //echo '</div>';
      					}
-              //else{ 
               if (substr( $_SESSION[$r], 0, 1 ) != "_"){
-                //echo '<div class="col-sm-5"';
-                //if (substr( $_SESSION[$r], 0, 1 ) != "_"){
-                //echo ' In 2nd if:   ';
+
                 echo $_SESSION[$r];
                 break;
-                //echo $temp_id;
-               // echo $xml->ballot[$district]->scopes->scope[$sc]->races->race[$t]->candidate[$n]->name;
-                //break;
-                //echo '</div>';
+
               }
-              //echo '</div>';
+              
      				}
      			}
-          //unset($_POST[$r]);
+         
 
      		}
         $dummy = $sc+1;
       }
-     		/*echo xml_attribute($xml->ballot[$district]->scopes->scope[$scope]->races->race[$r]->candidate, $_SESSION[$r]);*/
-     		/*$name = $xml->ballot[$district]->scopes->scope[$scope]->races->race[$r]->candidate->id[$_SESSION[$r]]->name;*/
-     		/*echo $name;*/
      		echo '<br>';
      	}
      	}
